8dcddf575136852ae638d205d8f4c1826869a197
Harish.K <harish2704@gmail.com>
2014-09-24 01:59:09 +0530

    Initial commit

---
+++ jobmanager-demo/lib.js
@@ -0,0 +1,187 @@
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"onOuVc":[function(require,module,exports){
+// ഓം  നമോ നാരായണായ 
+
+var utils = require('./utils');
+
+var STATE = utils.createEnum([ 'RUNNING' , 'NOT_RUNNING']);
+
+function JobManager(opts){
+
+    //opts
+    this.notifyAt = opts.notifyAt|| 3;
+    this.concurrency = opts.concurrency || 20;
+    this.minConcurrency = opts.minConcurrency != null ? opts.minConcurrency : 2;
+    
+    // state
+    this.state = STATE.NOT_RUNNING;
+
+    this.workers = [];
+    this.tasks = [];
+    this.work = null;
+    this.onLoadMore = null;
+    this.onError = null;
+    this.isLoadingTakingPlace = false;
+    this.endReached = false;
+    this.runningTasks = 0;
+}
+/*
+ * JobManager.prototype.__defineSetter__( 'workers', function(workers){
+ *     this.$workers = workers;
+ *     this.updateState();
+ * });
+ */
+
+JobManager.prototype.updateState = function(){
+    var workers = this.workers;
+    var tmpPoolBlock = workers.length &&  Math.ceil( this.concurrency/workers.length ) , i;
+    var tmpPool = [];
+    for( i=0; i < tmpPoolBlock; i++){
+        tmpPool = tmpPool.concat( this.workers );
+    }
+    this.tmpPool = tmpPool;
+    this.tmpPoolLen = tmpPoolBlock * this.workers.length ;
+};
+
+JobManager.prototype.setConcurrency = function( newVal ){
+    if( newVal > this.minConcurrency ){
+        if( newVal > this.tmpPoolLen ){
+            var tmpPool = this.tmpPool;
+            var additionalPoolBlocks = Math.ceil( ( newVal - this.tmpPoolLen )/this.workers.length ), i;
+            for( i=0; i < additionalPoolBlocks; i++){
+                tmpPool = tmpPool.concat( this.workers );
+            }
+            this.tmpPoolLen += additionalPoolBlocks * this.workers.length ;
+        }
+        this.concurrency = newVal;
+    }
+};
+
+/*
+ * JobManager.prototype.__defineGetter__( 'workers', function(){
+ *     return this.$workers;
+ * });
+ */
+
+JobManager.prototype.returnToPool = function (w){
+    this.runningTasks--;
+    this.tmpPool.push( w );
+};
+
+JobManager.prototype.getFromPool = function(){
+    // if( this.runningTasks < this.concurrency ){
+        this.runningTasks++;
+        return this.tmpPool.splice(0,1)[0];
+    // }
+};
+
+JobManager.prototype.$doWork_ = function( cb ){
+    var self = this;
+    var task = self.tasks.splice(0, 1)[0];
+    if( task == undefined ){
+        this.stop();
+        return;
+    }
+    var worker = self.getFromPool();
+    console.log( worker.name );
+    this.work( task, worker, function(err){
+        if(err && self.onError ){
+            self.onError(err, task, worker );
+        }
+        if(self.tasks.length/self.concurrency < self.notifyAt && (!self.isLoadingTakingPlace) ){
+            if( self.onLoadMore && !self.endReached ) {
+                self.$onLoadMore();
+            }
+        }
+        self.returnToPool( worker );
+        cb();
+        if( self.state == STATE.NOT_RUNNING  ){
+            if( ( self.runningTasks == 1 ) &&  self.onStopped ) { self.onStopped(); }
+        }
+    });
+};
+
+
+JobManager.prototype.$trigger = function (){
+    var self = this;
+    while( this.runningTasks < this.concurrency && this.state == STATE.RUNNING ){
+        this.$doWork_( function(){
+            self.$trigger();
+        });
+    }
+};
+
+JobManager.prototype.start = function( ) {
+    this.state = STATE.RUNNING;
+    this.updateState();
+    var self = this;
+    if( this.tasks.length ) {
+        return this.$trigger();
+    }
+    return this.$onLoadMore( function(){
+        self.$trigger();
+    });
+};
+
+JobManager.prototype.$onLoadMore = function( cb ){
+    cb = cb || function(){};
+    var self = this;
+    self.isLoadingTakingPlace = true;
+    self.onLoadMore(function(){
+        self.isLoadingTakingPlace = false;
+        return cb();
+    });
+};
+
+JobManager.prototype.pause = function(){
+    this.state = STATE.NOT_RUNNING;
+};
+
+JobManager.prototype.stop = function(){
+    this.state = STATE.NOT_RUNNING;
+};
+
+
+JobManager.prototype.resume = function(){
+    this.start();
+};
+
+exports.JobManager = JobManager;
+
+},{"./utils":3}],"./job-manager":[function(require,module,exports){
+module.exports=require('onOuVc');
+},{}],3:[function(require,module,exports){
+function clone(obj){
+    var newObj = {};
+    Object.keys(obj).forEach(function(k){ newObj[k] = obj[k]; });
+    return newObj;
+}
+
+function defaults( def, newValue ){
+    var out = clone( def );
+    if(newValue)
+    Object.keys(newValue).forEach(function(k){ out[k] = newValue[k]; });
+    return out;
+}
+function range(n){
+    var x=Array(n);
+    var i=0;
+    while(i<n){ x[i] = i;i++; }
+    return x;
+}
+
+function createEnum(a){
+    var out = {};
+    a.forEach(function(v, k){
+        out[v.toUpperCase()] = {index: k, name: v.toLowerCase() };
+    });
+    return out;
+}
+
+exports.clone = clone;
+exports.defaults = defaults;
+exports.range = range;
+exports.createEnum = createEnum;
+
+
+},{}]},{},[])
+;
---
+++ jobmanager-demo/test.js
@@ -0,0 +1,155 @@
+/*global $, document */
+
+var target = {
+    initialize : function(el){
+        this.el = el;
+        this.box = el.offset();
+    },
+    add: function(el){
+        this.el.append( el );
+    }
+};
+var source= {
+    initialize : function(el){
+        this.el = el;
+        this.box = el.offset();
+    },
+    take: function(){
+        var el = this.el[0].childNodes[ this.el[0].childNodes.length -1 ];
+        this.el[0].removeChild( el );
+        return el;
+    },
+    add: function(el){
+        this.el.append( el );
+    }
+};
+var restHouse, workers;
+
+var createTask = function( name ){
+    var el = $( '<div class="task" id="task-'+name+'"> T: '+name+' </div>' );
+    return el;
+};
+
+function Worker(parentEl, name){
+    var el = $( '<div class="worker" id="worker-'+name+'"> W: '+name+' </div>' ); 
+    this.el = el;
+    parentEl.append(el);
+    this.name = name;
+    this.speed = name +1;
+}
+Worker.prototype.putTask = function(){
+    this.el[0].removeChild( this.task[0] );
+    target.add(this.task );
+    return;
+};
+Worker.prototype.getTask = function(t){
+    // var t = source.take();
+    source.el[0].removeChild(t[0]);
+    this.task = t;
+    this.el.append(t);
+    return;
+};
+Worker.prototype.gotoSource = function( cb ){
+    this.el.css( this.el.offset() );
+    this.el.css( { position: 'fixed' } );
+    this.el.animate( source.box, 1000, cb );
+};
+var speed = 1;
+Worker.prototype.move = function( cb ){
+    var self = this;
+    this.el.animate( { left: '130px' }, this.speed*speed*1000, function(){
+        self.putTask();
+        var p = self.el.parent();
+        p[0].removeChild( self.el[0]);
+        p.append( self.el );
+        self.el.removeAttr('style');
+        if( cb ) cb();
+    });
+};
+Worker.prototype.doTask = function( t, cb) {
+    var self = this;
+    this.gotoSource(function(){
+        self.getTask( t );
+        self.move( cb );
+    });
+};
+
+var jm, JobManager, messageDiv;
+
+function flashMessage(message){
+    messageDiv.html( message);
+    messageDiv.slideDown(function() {
+        setTimeout(function() {
+            messageDiv.slideUp();
+        }, 5000);
+    });
+}
+
+$(document).ready( function(){
+    target.initialize ( $('#target') );
+    source.initialize ( $('#source') );
+    workers = [];
+    var j = 0;
+    function loadTask( cb ){
+        var tasks = [];
+        var i = 0;
+        while( i < 11 ) {
+            var t =  createTask(( j*11 ) + i);
+            source.add( t );
+            tasks.push( t );
+            i++;
+        }
+        j++;
+        if(cb)cb( null, tasks );
+    }
+    // loadTask();
+    restHouse = $('#restHouse');
+    [1,2,3,4,5,6,7].forEach( function(v){
+        workers.push( new Worker ( restHouse, v) );
+    });
+
+    messageDiv = $('#messages');
+    
+
+
+
+    JobManager = require('./job-manager').JobManager;
+    jm = new JobManager( { concurrency: 4});
+
+    jm.onLoadMore = function(cb){
+        flashMessage( 'onLoadMore()');
+        var self = this;
+        loadTask( function(err, tasks){
+            self.tasks = self.tasks.concat( tasks );
+            cb();
+        });
+    };
+    jm.workers = workers;
+    jm.work = function( task, worker, cb ){
+        worker.doTask(task, cb);
+    };
+});
+
+var startd = false;
+var buttonLabel = {
+    true: 'stop',
+    false: 'start'
+};
+function startStop(e){
+    startd = !startd;
+    $('#start').html( buttonLabel[startd]);
+    var concurrency = Number ( document.forms[0].concurrency.value ) || 4;
+    speed = Number ( document.forms[0].speed.value )  || 1;
+    if(startd){
+        jm.setConcurrency(concurrency);
+        jm.start();
+    } else {
+        jm.stop();
+    }
+    return false;
+}
+
+function updateConcurrency(){
+    var concurrency = Number ( document.forms[0].concurrency.value ) || 4;
+    jm.setConcurrency(concurrency);
+}
